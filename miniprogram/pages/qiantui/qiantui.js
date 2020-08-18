2c463697678575a2e92314245e52427f236c2dc1
unknown <unknown@example.com>
2020-08-18 13:14:03 +0800

    添加定位限制，实地测试

--- miniprogram/pages/qiantui/qiantui.js
+++ miniprogram/pages/qiantui/qiantui.js
@@ -45,15 +45,15 @@ Page({
       })
       return
     }
-    if (!this.pageData.locationObj.latitude || !this.pageData.locationObj.longitude) {
-      wx.showModal({
-        title: '错误',
-        content: '请打开定位！',
-        showCancel: false
-      })
-      return
-    }
-
+    if (this.pageData.locationObj.latitude < 43.82340 || this.pageData.locationObj.latitude > 43.82681 || this.pageData.locationObj.longitude < 125.26775 || this.pageData.locationObj.longitude > 125.27114) { 
+      wx.showModal({ 
+        title: '错误', 
+        content: '检测到地理位置错误！', 
+        showCancel: false 
+      }) 
+      return 
+    } 
+    
     qiantui.add({
       data: {
         name: event.detail.value.name,
